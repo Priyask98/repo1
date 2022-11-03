02bdbf7b26354ec3c420ed12bebb46f31b93dde7
priya <priyankaskalasappanavr@gmail.com>
2022-11-03 13:00:49 +0530

    first commit to cherrypicbranch

--- gitJavaPractice/src/com/sync/gitPractice/GitPractice.java
+++ gitJavaPractice/src/com/sync/gitPractice/GitPractice.java
@@ -4,7 +4,7 @@ public class GitPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome.......to synchronosss...");
+		System.out.println("Welcome....to synchronosss...cherrypic");
 
 	}
 
