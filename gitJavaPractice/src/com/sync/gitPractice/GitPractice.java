8c16d61fd05c827a197119220ea8636a9d9e0116
priya <priyankaskalasappanavr@gmail.com>
2022-11-07 11:59:25 +0530

    stashed file

--- gitJavaPractice/src/com/sync/gitPractice/GitPractice.java
+++ gitJavaPractice/src/com/sync/gitPractice/GitPractice.java
@@ -4,8 +4,8 @@ public class GitPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Welcome....to synchronosss...cherrypic .........conflict");
 
+		System.out.println("Welcome....to synchronosss...stash..");
 
 	}
 
